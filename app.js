d7a0208656d183084473da28fb7b68339d5743c2
piyushbind174-source <piyushbind174@gmail.com>
2026-02-04 08:59:52 +0530

    Added Email Validation

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://cashinvoice-frontend.onrender.com/"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
